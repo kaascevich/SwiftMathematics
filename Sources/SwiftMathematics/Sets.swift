3794282a276371c19ca8ecdea4046e7d9f86e7de
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-15 13:48:44 -0400

    add documentation to set membership operators

--- Sources/SwiftMathematics/Sets.swift
+++ Sources/SwiftMathematics/Sets.swift
@@ -32,20 +32,104 @@ infix operator ∋: ComparisonPrecedence
 infix operator ∌: ComparisonPrecedence
 
 public extension Set {
-    static func ∈ (element: Element, sequence: Self) -> Bool {
-        sequence.contains(element)
+    /// Returns a Boolean value that indicates whether the given element exists
+    /// in the given set.
+    ///
+    /// This example uses the `∈` operator to test whether an integer is a member
+    /// of a set of prime numbers.
+    ///
+    ///     let primes: Set = [2, 3, 5, 7]
+    ///     let x = 5
+    ///     if x ∈ primes {
+    ///         print("\(x) is prime!")
+    ///     } else {
+    ///         print("\(x). Not prime.")
+    ///     }
+    ///     // Prints "5 is prime!"
+    ///
+    /// - Parameters:
+    ///    - element: An element to look for in the set.
+    ///    - set: A set.
+    /// - Returns: `true` if `element` exists in the set; otherwise, `false`.
+    ///
+    /// - Complexity: O(1)
+    static func ∈ (element: Element, set: Self) -> Bool {
+        set.contains(element)
     }
     
-    static func ∉ (element: Element, sequence: Self) -> Bool {
-        !(element ∈ sequence)
+    /// Returns a Boolean value that indicates whether the given element does not
+    /// exist in the given set.
+    ///
+    /// This example uses the `∉` operator to test whether an integer is a member
+    /// of a set of prime numbers.
+    ///
+    ///     let primes: Set = [2, 3, 5, 7]
+    ///     let x = 4
+    ///     if x ∉ primes {
+    ///         print("\(x). Not prime.")
+    ///     } else {
+    ///         print("\(x) is prime!")
+    ///     }
+    ///     // Prints "5. Not prime."
+    ///
+    /// - Parameters:
+    ///    - element: An element to look for in the set.
+    ///    - set: A set.
+    /// - Returns: `true` if `element` does not exist in the set; otherwise, `false`.
+    ///
+    /// - Complexity: O(1)
+    static func ∉ (element: Element, set: Self) -> Bool {
+        !(element ∈ set)
     }
     
-    static func ∋ (sequence: Self, element: Element) -> Bool {
-        sequence.contains(element)
+    /// Returns a Boolean value that indicates whether the given element exists
+    /// in the given set.
+    ///
+    /// This example uses the `∋` operator to test whether an integer is a member
+    /// of a set of prime numbers.
+    ///
+    ///     let primes: Set = [2, 3, 5, 7]
+    ///     let x = 5
+    ///     if primes ∋ x {
+    ///         print("\(x) is prime!")
+    ///     } else {
+    ///         print("\(x). Not prime.")
+    ///     }
+    ///     // Prints "5 is prime!"
+    ///
+    /// - Parameters:
+    ///    - set: A set.
+    ///    - element: An element to look for in the set.
+    /// - Returns: `true` if `element` exists in the set; otherwise, `false`.
+    ///
+    /// - Complexity: O(1)
+    static func ∋ (set: Self, element: Element) -> Bool {
+        set.contains(element)
     }
     
-    static func ∌ (sequence: Self, element: Element) -> Bool {
-        !(sequence ∋ element)
+    /// Returns a Boolean value that indicates whether the given element does not
+    /// exist in the given set.
+    ///
+    /// This example uses the `∌` operator to test whether an integer is a member
+    /// of a set of prime numbers.
+    ///
+    ///     let primes: Set = [2, 3, 5, 7]
+    ///     let x = 4
+    ///     if primes ∌ x {
+    ///         print("\(x). Not prime.")
+    ///     } else {
+    ///         print("\(x) is prime!")
+    ///     }
+    ///     // Prints "5. Not prime."
+    ///
+    /// - Parameters:
+    ///    - set: A set.
+    ///    - element: An element to look for in the set.
+    /// - Returns: `true` if `element` does not exist in the set; otherwise, `false`.
+    ///
+    /// - Complexity: O(1)
+    static func ∌ (set: Self, element: Element) -> Bool {
+        !(set ∋ element)
     }
 }
 
@@ -58,24 +142,24 @@ infix operator ⊈: ComparisonPrecedence
 infix operator ⊊: ComparisonPrecedence
 
 public extension Set {
-    static func ⊂ (subset: Self, sequence: Self) -> Bool {
-        subset.isStrictSubset(of: sequence)
+    static func ⊂ (subset: Self, set: Self) -> Bool {
+        subset.isStrictSubset(of: set)
     }
     
-    static func ⊄ (subset: Self, sequence: Self) -> Bool {
-        !(subset ⊂ sequence)
+    static func ⊄ (subset: Self, set: Self) -> Bool {
+        !(subset ⊂ set)
     }
     
-    static func ⊆ (subset: Self, sequence: Self) -> Bool {
-        subset.isSubset(of: sequence)
+    static func ⊆ (subset: Self, set: Self) -> Bool {
+        subset.isSubset(of: set)
     }
     
-    static func ⊈ (subset: Self, sequence: Self) -> Bool {
-        !(subset ⊆ sequence)
+    static func ⊈ (subset: Self, set: Self) -> Bool {
+        !(subset ⊆ set)
     }
     
-    static func ⊊ (subset: Self, sequence: Self) -> Bool {
-        subset ⊂ sequence && subset != sequence
+    static func ⊊ (subset: Self, set: Self) -> Bool {
+        subset ⊂ set && subset != set
     }
 }
 
@@ -88,23 +172,23 @@ infix operator ⊉: ComparisonPrecedence
 infix operator ⊋: ComparisonPrecedence
 
 public extension Set {
-    static func ⊃ (superset: Self, sequence: Self) -> Bool {
-        superset.isStrictSuperset(of: sequence)
+    static func ⊃ (superset: Self, set: Self) -> Bool {
+        superset.isStrictSuperset(of: set)
     }
     
-    static func ⊅ (superset: Self, sequence: Self) -> Bool {
-        !(superset ⊃ sequence)
+    static func ⊅ (superset: Self, set: Self) -> Bool {
+        !(superset ⊃ set)
     }
     
-    static func ⊇ (superset: Self, sequence: Self) -> Bool {
-        superset.isSuperset(of: sequence)
+    static func ⊇ (superset: Self, set: Self) -> Bool {
+        superset.isSuperset(of: set)
     }
     
-    static func ⊉ (superset: Self, sequence: Self) -> Bool {
-        !(superset ⊇ sequence)
+    static func ⊉ (superset: Self, set: Self) -> Bool {
+        !(superset ⊇ set)
     }
     
-    static func ⊋ (superset: Self, sequence: Self) -> Bool {
-        superset ⊃ sequence && superset != sequence
+    static func ⊋ (superset: Self, set: Self) -> Bool {
+        superset ⊃ set && superset != set
     }
 }
